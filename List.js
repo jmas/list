83a76d7eff20e038d49a2796d987ba29c8e9d971
Alexander Maslakov <alexandr@ego-cms.com>
2014-09-25 01:03:12 +0300

    Init

---
+++ List.js
@@ -0,0 +1,67 @@
+(function(rootScope) {
+  var mustache = require('minstache');
+  var throttle = require('throttle');
+
+  var List = function(data, el, itemTamplate) {
+    if (! data instanceof Arr) {
+      throw new Error('data should be an instance of Arr');
+    }
+   
+    if (! el instanceof HTMLElement) {
+      throw new Error('el should be an instanceof of HTMLElement');
+    }
+   
+    if (typeof itemTamplate !== 'string') {
+      throw new Error('itemTamplate should be a string');
+    }
+   
+    if (typeof mustache === 'undefined') {
+      throw new Error('mustache library is required');
+    }
+   
+    if (typeof throttle === 'undefined') {
+      throw new Error('throttle library is required');
+    }
+   
+    this.data = data;
+    this.el = el;
+    this.itemTamplate = itemTamplate;
+   
+    this.data.on('change', throttle(this.render.bind(this), 300));
+    
+    if (this.data.length > 0) {
+      this.render();
+    }
+  };
+   
+  List.prototype = {
+    //
+    clean: function() {
+      while (this.el.firstChild) {
+          this.el.removeChild(this.el.firstChild);
+      }
+   
+      return this;
+    },
+   
+    //
+    render: function() {
+      this.clean();
+   
+      if (this.data.length > 0) {
+        for (var i=0,len=this.data.length; i<len; i++) {
+          this.el.insertAdjacentHTML('beforeend', mustache(this.itemTamplate, this.data[i]));
+        }
+      }
+      
+      return this;
+    }
+  };
+
+  if (typeof module !== 'undefined') {
+    module.exports = throttle;
+  } else {
+    rootScope.throttle = throttle;
+  }
+
+})(this);
---
+++ component.js
@@ -0,0 +1,15 @@
+{
+  "name": "list",
+  "repository": "jmas/list",
+  "description": "List component that use Arr.js and throttle.js",
+  "version": "0.0.1",
+  "keywords": ["list"],
+  "dependencies": {
+    "jmas/arr": "*",
+    "jmas/throttle": "*",
+    "visionmedia/minstache": "*"
+  },
+  "scripts": ["List.js"],
+  "main": "List.js",
+  "license": "MIT"
+}
